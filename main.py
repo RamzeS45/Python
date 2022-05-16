ae78e5d7b23cf7d2b77d3dc112b4d14ea405d62e
RamzeS45 <roma.guchanov@mail.ru>
2022-05-16 14:40:54 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,13 @@
+class Refer:
+    data = []
+    def __init__(self, data):
+        self.__data = data[1]
+
+    def diam (self, data):
+        data.map()
+        data[0] + 1
+        return
+
+x = Refer ([1,2])
+print(x.diam)
+
